aa3fe9bb044a7a3b9d28ebaf3e5f8f689e73e9e9
himanshig05 <ghiman8215@gmail.com>
2025-03-31 13:32:52 +0530

    pages made

--- frontend/src/Network.jsx
+++ frontend/src/Network.jsx
@@ -1,14 +1,14 @@
 import Navbar from "./components/NavBar";
 
-const Home = () => {
+const Network = () => {
   return (
     <>
       <Navbar />
       <div className="page-container">
-        <h2>Welcome to Home Page</h2>
+        <h2>Welcome to Network Page</h2>
       </div>
     </>
   );
 };
 
-export default Home;
+export default Network;
