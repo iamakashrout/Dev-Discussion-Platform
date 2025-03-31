526697e6cce0a8d58a3393ea242382caf4382f0d
himanshig05 <ghiman8215@gmail.com>
2025-03-31 13:33:07 +0530

    login page updated

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import "./Login.css"; 
 import image from "./assets/image.png"; 
+
 const BASE_URL = "http://localhost:5000";
 
 const Login = ({ setAuth }) => {
@@ -16,9 +17,9 @@ const Login = ({ setAuth }) => {
       const response = await axios.post(`${BASE_URL}/auth/login`, { emailid, password });
       if (response.data.success) {
         localStorage.setItem("token", response.data.token);
-        setAuth(true);
+        setAuth(true);  
         alert("Login successful!");
-        navigate("/");
+        navigate("/home");
       } else {
         alert(response.data.message);
       }
@@ -29,18 +30,17 @@ const Login = ({ setAuth }) => {
 
   return (
     <div className="register-container">
-          {/* Left Image Section */}
-          <div className="register-image">
-  <img src={image} alt="Welcome" />
-  <div className="welcome-text">
-    <h1>
-      <span>Welcome back to </span>
-      <span style={{ color:  "#8a2be2"}}>Dev</span>
-      <span style={{ color: "black" }}>Sphere</span>!
-    </h1>
-    <h3>You can sign in to access your existing account.</h3>
-  </div>
-</div>
+      <div className="register-image">
+        <img src={image} alt="Welcome" />
+        <div className="welcome-text">
+          <h1>
+            <span>Welcome back to </span>
+            <span style={{ color: "#8a2be2" }}>Dev</span>
+            <span style={{ color: "black" }}>Sphere</span>!
+          </h1>
+          <h3>You can sign in to access your existing account.</h3>
+        </div>
+      </div>
 
       <div className="register-form">
         <form onSubmit={handleLogin}>
@@ -56,3 +56,65 @@ const Login = ({ setAuth }) => {
 };
 
 export default Login;
+
+//older code:
+// import { useState } from "react";
+// import { useNavigate, Link } from "react-router-dom";
+// import axios from "axios";
+// import "./Login.css"; 
+// import image from "./assets/image.png"; 
+// const BASE_URL = "http://localhost:5000";
+
+// const Login = ({ setAuth }) => {
+//   const [emailid, setEmailId] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate();
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.post(`${BASE_URL}/auth/login`, { emailid, password });
+//       if (response.data.success) {
+//         localStorage.setItem("token", response.data.token);
+//         setAuth(true);
+//         alert("Login successful!");
+//         navigate("/");
+//       } else {
+//         alert(response.data.message);
+//       }
+//     } catch (error) {
+//       alert("There has been an error, try again!");
+//     }
+//   };
+
+//   return (
+//     <div className="register-container">
+//           {/* Left Image Section */}
+//           <div className="register-image">
+//   <img src={image} alt="Welcome" />
+//   <div className="welcome-text">
+//     <h1>
+//       <span>Welcome back to </span>
+//       <span style={{ color:  "#8a2be2"}}>Dev</span>
+//       <span style={{ color: "black" }}>Sphere</span>!
+//     </h1>
+//     <h3>You can sign in to access your existing account.</h3>
+//   </div>
+// </div>
+
+//       <div className="register-form">
+//         <form onSubmit={handleLogin}>
+//           <h2>Login</h2>
+//           <input type="email" placeholder="Email" value={emailid} onChange={(e) => setEmailId(e.target.value)} required />
+//           <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+//           <button type="submit">Login</button>
+//           <p>Not a member? <Link to="/register" className="login-link">Register here</Link></p>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+
